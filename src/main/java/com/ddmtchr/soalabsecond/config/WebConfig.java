081eb505796bf0da455254f78275751c1482b2f0
Mikhail Romanenko <rmr04489@gmail.com>
2025-11-17 10:58:05 +0300

    cors

--- src/main/java/com/ddmtchr/soalabsecond/config/WebConfig.java
+++ src/main/java/com/ddmtchr/soalabsecond/config/WebConfig.java
@@ -21,15 +21,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://localhost:3000",
-                        "http://se.ifmo.ru",
-                        "https://se.ifmo.ru"
-                )
+                .allowedOrigins("https://se.ifmo.ru")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
